477e5763189da5baf9c18510293e0d012cae214b
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-11-09 13:36:25 +0100

    Update synth.rb
        Slide Detection (slide_detected?): Determines if a frequency change is a slide based on a threshold difference in MIDI notes.
    
        Slide Handling (handle_slide): Interpolates MIDI notes over a set duration to smoothly transition between notes.
    
        Frequency Setting (frequency=): Updates frequency and controls slides or discrete note changes.
    
        ADSR Envelope Adjustment (adjust_ads): Adjusts the attack, decay, and sustain lengths based on the current state.
    
        Converting SID Frequency to MIDI (sid_frequency_to_nearest_midi, nearest_tone, sid_frequency_to_actual_frequency): Converts SID chip frequencies to the nearest MIDI note.
    
        State Management (release!, stop!): Manages the synth's state, including handling release and stop events.

--- lib/sidtool/synth.rb
+++ lib/sidtool/synth.rb
@@ -1,22 +1,29 @@
 module Sidtool
   class Synth
     attr_reader :start_frame
-    attr_accessor :waveform
-    attr_accessor :attack
-    attr_accessor :decay
-    attr_reader :sustain_length
+    attr_accessor :waveform, :attack, :decay
+    attr_reader :sustain_length, :controls
     attr_accessor :release
-    attr_reader :controls
+
+    SLIDE_THRESHOLD = 5 # Threshold for detecting slides
+    SLIDE_DURATION_FRAMES = 20 # Duration over which to spread the slide
 
     def initialize(start_frame)
       @start_frame = start_frame
       @controls = []
+      @frequency = nil
+      @released_at = nil
     end
 
     def frequency=(frequency)
       if @frequency
         previous_midi, current_midi = sid_frequency_to_nearest_midi(@frequency), sid_frequency_to_nearest_midi(frequency)
-        @controls << [STATE.current_frame, current_midi] if previous_midi != current_midi
+
+        if slide_detected?(@frequency, frequency)
+          handle_slide(previous_midi, current_midi)
+        else
+          @controls << [STATE.current_frame, current_midi] if previous_midi != current_midi
+        end
       end
       @frequency = frequency
     end
@@ -25,16 +32,8 @@ module Sidtool
       return if released?
 
       @released_at = STATE.current_frame
-      length_of_attack_decay_sustain = (STATE.current_frame - @start_frame) / FRAMES_PER_SECOND
-      if length_of_attack_decay_sustain < @attack
-        @attack = length_of_attack_decay_sustain
-        @decay, @sustain_length = 0, 0
-      elsif length_of_attack_decay_sustain < @attack + @decay
-        @decay = length_of_attack_decay_sustain - @attack
-        @sustain_length = 0
-      else
-        @sustain_length = length_of_attack_decay_sustain - @attack - @decay
-      end
+      length_of_ads = (STATE.current_frame - @start_frame) / FRAMES_PER_SECOND
+      @attack, @decay, @sustain_length = adjust_ads(length_of_ads)
     end
 
     def released?
@@ -59,22 +58,41 @@ module Sidtool
     end
 
     private
+
+    def slide_detected?(old_frequency, new_frequency)
+      old_midi, new_midi = sid_frequency_to_nearest_midi(old_frequency), sid_frequency_to_nearest_midi(new_frequency)
+      (new_midi - old_midi).abs > SLIDE_THRESHOLD
+    end
+
+    def handle_slide(start_midi, end_midi)
+      midi_step = (end_midi - start_midi) / SLIDE_DURATION_FRAMES.to_f
+      (1..SLIDE_DURATION_FRAMES).each do |frame_offset|
+        interpolated_midi = start_midi + (midi_step * frame_offset)
+        @controls << [STATE.current_frame + frame_offset, interpolated_midi.round]
+      end
+    end
+
+    def adjust_ads(length_of_ads)
+      if length_of_ads < @attack
+        [length_of_ads, 0, 0]
+      elsif length_of_ads < @attack + @decay
+        [@attack, length_of_ads - @attack, 0]
+      else
+        [@attack, @decay, length_of_ads - @attack - @decay]
+      end
+    end
+
     def sid_frequency_to_nearest_midi(sid_frequency)
       actual_frequency = sid_frequency_to_actual_frequency(sid_frequency)
       nearest_tone(actual_frequency)
     end
 
     def nearest_tone(frequency)
-      # Stolen from Sonic Pi
       midi_tone = (12 * (Math.log(frequency * 0.0022727272727) / Math.log(2))) + 69
-
       midi_tone.round
     end
 
     def sid_frequency_to_actual_frequency(sid_frequency)
-      # With a standard 1 MHz clock
-      # (sid_frequency * 0.0596).round(2)
-      # PAL clock: 985248
       (sid_frequency * (CLOCK_FREQUENCY / 16777216)).round(2)
     end
   end
